53ac0b6523749bbfd39ad7cc9b1067b4504453d3
Taufik Nurrohman <t.nurrohman77@gmail.com>
2017-01-04 11:32:01 +0700

    Moved to a Separate Repository

--- lot/extend/plugin/lot/asset/block-e/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-Block::set('e', function($content) {
-    return Block::replace('e', function($content) {
-        ob_start();
-        eval($content);
-        return ob_get_clean();
-    }, $content);
-});
