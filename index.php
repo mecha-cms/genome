1a7949d6aa768f9ac5dbe565e5dcf16c1058b5f7
Taufik Nurrohman <t.nurrohman77@gmail.com>
2017-05-11 13:01:56 +0700

    Update

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
  */
 
 if (!defined('DS')) define('DS', DIRECTORY_SEPARATOR);
-if (!defined('ROOT')) define('ROOT', rtrim(__DIR__, DS));
+if (!defined('ROOT')) define('ROOT', __DIR__);
 
 if (!defined('DENT')) define('DENT', '  '); // Default HTML indent
 if (!defined('N')) define('N', "\n"); // Line break
@@ -19,10 +19,10 @@ if (!defined('X')) define('X', "\x1A"); // Placeholder text
 if (!defined('SESSION')) define('SESSION', null); // Change to a folder path to define `session_save_path`
 if (!defined('DEBUG')) define('DEBUG', null); // Change to `true` to enable debug mode
 
-if (!defined('ENGINE')) define('ENGINE', ROOT . DS . 'engine');
-if (!defined('LOT')) define('LOT', ROOT . DS . 'lot');
+if (!defined('ENGINE')) define('ENGINE', __DIR__ . DS . 'engine');
+if (!defined('LOT')) define('LOT', __DIR__ . DS . 'lot');
 
-foreach (glob(LOT . DS . '*', GLOB_NOSORT | GLOB_ONLYDIR) as $lot) {
+foreach (glob(__DIR__ . DS . 'lot' . DS . '*', GLOB_NOSORT | GLOB_ONLYDIR) as $lot) {
     $s = strtoupper(str_replace(['-', '.'], ['_', '__'], basename($lot)));
     if (!defined($s)) {
         define($s, $lot);
@@ -40,7 +40,7 @@ if (!defined('DATE_WISE')) define('DATE_WISE', 'Y-m-d H:i:s');
 // Common file type(s) allowed to be uploaded by the file manager
 if (!defined('FONT_X')) define('FONT_X', 'eot,otf,svg,ttf,woff,woff2');
 if (!defined('IMAGE_X')) define('IMAGE_X', 'bmp,cur,gif,ico,jpeg,jpg,png,svg');
-if (!defined('MEDIA_X')) define('MEDIA_X', '3gp,avi,flv,mkv,mov,mp3,mp4,m4a,m4v,ogg,swf,wav,wma');
+if (!defined('MEDIA_X')) define('MEDIA_X', '3gp,avi,flv,mkv,mov,mp3,mp4,m4a,m4v,ogg,swf,wav,webm,wma');
 if (!defined('PACKAGE_X')) define('PACKAGE_X', 'gz,iso,rar,tar,zip,zipx');
 if (!defined('SCRIPT_X')) define('SCRIPT_X', 'archive,cache,css,data,draft,htaccess,html,js,json,log,page,php,stack,trash,txt,xml,yaml,yml');
 
