29f8998740c5695dd3b330daf52ab672db37b3a1
Taufik Nurrohman <t.nurrohman77@gmail.com>
2017-12-31 19:20:31 +0700

    Update

--- lot/shield/document/page.php
+++ lot/shield/document/page.php
@@ -6,12 +6,12 @@
       <h2><span><?php echo $page->title; ?></span></h2>
       <p><strong><?php echo $language->author; ?>:</strong> <?php echo $page->author; ?></p>
     </header>
-    <section>
+    <div>
       <?php echo $page->content; ?>
       <?php if ($page->link): ?>
       <p><a href="<?php echo $page->link; ?>" rel="nofollow" target="_blank"><?php echo $language->link; ?> &#x21E2;</a></p>
       <?php endif; ?>
-    </section>
+    </div>
     <footer>
       <?php $update = new Date($page->update); ?>
       <p><strong><?php echo $language->updateed; ?>:</strong> <time datetime="<?php echo $update->W3C; ?>"><?php echo $update->{str_replace('-', '_', $config->language)}; ?></time></p>
