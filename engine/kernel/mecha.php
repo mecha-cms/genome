944c099cac55e794b7b899b9323e2e78480582d4
Taufik Nurrohman <t.nurrohman77@gmail.com>
2018-03-29 14:04:39 +0700

    API Change(s)

--- engine/kernel/mecha.php
+++ engine/kernel/mecha.php
@@ -3,7 +3,7 @@
 class Mecha extends Genome {
 
     // Current version
-    const version = '2.0.1';
+    const version = '2.1.0';
 
     // Compare with current version
     public static function version($v = null, $c = null) {
