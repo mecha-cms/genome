8b3728f5a75a6f085482d7d9a7c18cd58a9a2b36
Taufik Nurrohman <t.nurrohman77@gmail.com>
2018-04-15 20:43:40 +0700

    Rename `warning` to `warn`

--- engine/plug/message.php
+++ engine/plug/message.php
@@ -1,6 +1,6 @@
 <?php
 
-foreach (['error', 'warning'] as $kin) {
+foreach (['error', 'warn'] as $kin) {
     Message::_($kin, function($text, $vars = [], $preserve_case = false) use($kin) {
         ++Message::$x;
         return Message::set($kin, $text, $vars, $preserve_case);
