f7b4184eabf5e1e3be9dc8c6c5cfa1b0114d35c9
Taufik Nurrohman <t.nurrohman77@gmail.com>
2018-10-02 10:51:06 +0700

    Allow to Disable Safe Data Override
    If the first argument is an array, and by setting the second argument to `false`, it will change the behaviour from `$foo = array_replace_recursive($foo, [ ... ])` to `$foo = [ ... ]`

--- engine/kernel/set.php
+++ engine/kernel/set.php
@@ -8,9 +8,16 @@ class Set extends Genome {
             $key = array_shift($lot);
             $value = array_shift($lot);
             if (is_array($key)) {
-                foreach ($key as $k => $v) {
-                    Anemon::set($GLOBALS, $id . '.' . $k, $v);
+                // `Set::post(['foo' => 'bar'], false)`
+                if ($value === false) {
+                    $GLOBALS[$id] = $key;
+                // `Set::post(['foo' => 'bar'])`
+                } else {
+                    foreach ($key as $k => $v) {
+                        Anemon::set($GLOBALS, $id . '.' . $k, $v);
+                    }
                 }
+            // `Set::post('foo', 'bar')`
             } else {
                 Anemon::set($GLOBALS, $id . '.' . $key, $value);
             }
