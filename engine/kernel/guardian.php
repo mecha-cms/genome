28098d5aad51d93812791fdd783cac21445b284b
Taufik Nurrohman <t.nurrohman77@gmail.com>
2018-10-02 10:48:01 +0700

    Hook as Filter

--- engine/kernel/guardian.php
+++ engine/kernel/guardian.php
@@ -32,9 +32,9 @@ class Guardian extends Genome {
         if (!isset($path)) {
             $path = $current;
         }
-        $long = URL::long($path, false);
         Session::set('url.previous', $current);
-        Hook::fire(__c2f__(static::class, '_', '/') . '.' . __FUNCTION__, [$long, $path]);
+        $long = URL::long($path, false);
+        $long = Hook::fire(__c2f__(static::class, '_', '/') . '.' . __FUNCTION__, [$long, $path]);
         header('Location: ' . str_replace('&amp;', '&', $long));
         exit;
     }
