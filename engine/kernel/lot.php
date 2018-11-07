f1c667aaaa1fd0ccc68054ba64c0b6950cbe7c0a
Taufik Nurrohman <t.nurrohman77@gmail.com>
2018-11-07 11:57:56 +0700

    Remove `$scope` Option
    Simply use the class name.

--- engine/kernel/lot.php
+++ engine/kernel/lot.php
@@ -2,29 +2,28 @@
 
 class Lot extends Genome {
 
-    public static function set($key, $value = null, $scope = null) {
+    public static function set($key, $value = null) {
         if (is_array($key) || is_object($key)) {
-            $scope = '.' . md5($value ?: static::class);
+            $scope = '.' . md5(static::class);
             foreach ($key as $k => $v) {
                 $GLOBALS[$scope][$k] = $v;
             }
         } else {
-            $GLOBALS['.' . md5($scope ?: static::class)][$key] = $value;
+            $GLOBALS['.' . md5(static::class)][$key] = $value;
         }
         return new static;
     }
 
-    public static function get($key = null, $fail = false, $scope = null) {
-        $scope = '.' . md5($scope ?: static::class);
-        $data = $GLOBALS[$scope] ?? [];
+    public static function get($key = null, $fail = false) {
+        $data = $GLOBALS['.' . md5(static::class)] ?? [];
         if (isset($key)) {
             return array_key_exists($key, $data) ? $data[$key] : $fail;
         }
         return $data ?: $fail;
     }
 
-    public static function reset($key = null, $scope = null) {
-        $scope = '.' . md5($scope ?: static::class);
+    public static function reset($key = null) {
+        $scope = '.' . md5(static::class);
         if (isset($key)) {
             if (is_array($key)) {
                 foreach ($key as $v) {
