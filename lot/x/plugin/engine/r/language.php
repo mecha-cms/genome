8de7be8dbc67f361c894405017de887c67151821
Taufik Nurrohman <t.nurrohman77@gmail.com>
2019-07-17 10:22:02 +0700

    #57

--- lot/x/plugin/engine/r/language.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-Language::set([
-    'plugin' => ['Plugin', 'Plugin', 'Plugins'],
-    'plugin-count' => function(int $i) {
-        return $i . ' Plugin' . ($i === 1 ? "" : 's');
-    }
-]);
