863c1a87b4f984640b65d69eb869c527d1a6a39b
Taufik Nurrohman <t.nurrohman77@gmail.com>
2019-07-27 00:06:24 +0700

    Add Published Date
    Remove author appearance, as this CMS is mostly used personally so everybody will know exactly who write what.

--- lot/content/document/page.php
+++ lot/content/document/page.php
@@ -3,8 +3,8 @@
   <article id="page-<?php echo $page->id; ?>">
     <header>
       <h2><span><?php echo $page->title; ?></span></h2>
-      <?php if ($page->author): ?>
-      <p><strong><?php echo $language->author; ?>:</strong> <?php echo $page->author; ?></p>
+      <?php if ($site->has('parent')): ?>
+      <p><time datetime="<?php echo $page->time->ISO8601; ?>"><?php echo $page->time->{r('-', '_', $site->language)}; ?></time></p>
       <?php endif; ?>
     </header>
     <div>
@@ -13,9 +13,6 @@
       <p><a href="<?php echo $page->link; ?>" rel="nofollow" target="_blank"><?php echo $language->link; ?> &#x21E2;</a></p>
       <?php endif; ?>
     </div>
-    <footer>
-      <p><strong><?php echo $language->update; ?>:</strong> <time datetime="<?php echo $page->update->ISO8601; ?>"><?php echo $page->update->{r('-', '_', $site->language)}; ?></time></p>
-    </footer>
   </article>
 </main>
 <?php if ($site->has('parent')): ?>
